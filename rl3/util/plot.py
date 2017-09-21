7a9b8e0dce8dfdb93edf02a2824ec3b2dcfa5949
Leandro Ferrado <ljferrado@gmail.com>
2017-09-21 01:00:44 -0300

    Refactor on plot function

--- rl3/util/plot.py
+++ rl3/util/plot.py
@@ -9,13 +9,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_moving_avg(total_rewards, show=True):
+def plot_moving_avg(total_rewards, title='', show=True):
     # See https://github.com/lazyprogrammer/machine_learning_examples/blob/master/rl2/cartpole/q_learning_bins.py
     N = len(total_rewards)
     moving_avg = np.empty(N)
     for t in range(N):
         moving_avg[t] = total_rewards[max(0, t-100):(t+1)].mean()
     plt.plot(moving_avg)
-    plt.title("Total rewards - moving average")
+    plt.title(title)
     if show:
         plt.show()
